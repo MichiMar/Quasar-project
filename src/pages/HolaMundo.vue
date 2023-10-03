ee122612704d2c16b24380d17cf98844be0e7d2f
MichiMar <michimar.alexis@gmail.com>
2023-10-02 23:07:08 -0600

    Created smaple route

---
+++ src/pages/HolaMundo.vue
@@ -0,0 +1,6 @@
+<template>
+  <q-page>
+    <h2>Hola Mundo</h2>
+  </q-page>
+</template>
+<script setup></script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,11 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'holamundo', component: () => import('pages/HolaMundo.vue') }
+
     ]
   },
 
